3e87363d3b6055e25100fcdde46bdb24d5a680a2
motoha <motoha2010@gmail.com>
2025-01-05 07:41:21 +0700

    up

--- src/app.module.ts
+++ src/app.module.ts
@@ -15,11 +15,18 @@ import { CartModule } from './cart/cart.module';
 import { OrderController } from './order/order.controller';
 import { OrderService } from './order/order.service';
 import { OrderModule } from './order/order.module';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { join } from 'path';
 @Module({
 
   imports: [UserModule, PropertyModule,OrderModule, MessageModule,CategoriesModule, CartModule, ProductModule,ProductEcomModule, MulterModule.register({
     dest: './uploads', // directory to save uploaded files
-  }),],
+  }),
+  ServeStaticModule.forRoot({
+    rootPath: join(__dirname, '..', 'uploads'), // Path to the uploads directory
+    serveRoot: '/nestjs-ecommerce-withAI/uploads', // URL path to serve files from
+  }),
+],
   controllers: [AppController   ],
  
 })
